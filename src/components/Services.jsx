cb9b3a1ff42154e03c6d9e330d363210212cd4f9
fireblack32 <jhondel1985@gmail.com>
2025-04-08 00:41:42 -0500

    Primera Parte Lista para Hosting, pendiente seccion de coches

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,7 +11,7 @@ const services = [
     { id: 1, name: "Categoría A2", description: "Habilitación para conducir motocicletas de alto cilindraje.", src: image1 },
     { id: 2, name: "Categoría B1", description: "Licencia para conducir vehículos particulares de hasta 9 pasajeros.", src: image2 },
     { id: 3, name: "Categoría C1", description: "Autorización para conducir taxis y vehículos de servicio público.", src: image3 },
-    { id: 4, name: "Re-categorización B1 a C1", description: "Proceso de actualización para conductores que desean manejar transporte público.", src: image4 },
+    { id: 4, name: "Re-categorización B1 a C1", description: "Proceso de actualización para conductores de transporte público.", src: image4 },
     { id: 5, name: "Refuerzo práctico de Carro", description: "Sesiones prácticas para mejorar habilidades de conducción en carro.", src: image5 },
     { id: 6, name: "Refuerzo práctico de Moto", description: "Clases personalizadas para perfeccionar el manejo de motocicletas.", src: image6 }
 ];
@@ -22,8 +22,8 @@ export default function ServicesSection() {
             <h2 className="services-title">NUESTROS SERVICIOS</h2>
             <div className="services-container">
                 {services.map((service) => (
-                    <div key={service.id} className="service-card">
-                        <img src={service.src} alt={service.name} className="service-image" />
+                    <div key={service.id} className="service-card" style={{ backgroundColor: "#D2D4D4" }}>
+                        <img src={service.src} alt={service.name} className="service-image" style={{mixBlendMode: "multiply"}} />
                         <h3 className="service-name">{service.name}</h3>
                         <p className="service-description">{service.description}</p>
                     </div>
